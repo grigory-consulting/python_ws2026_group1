a5a4baf29c96422342609a035e903c7c4a8e2436
grigory-consulting <consulting@vadze.de>
2026-01-19 13:16:15 +0100

    commit mon 19-Jan

--- tutorial_advanced/classes.py
+++ tutorial_advanced/classes.py
@@ -27,14 +27,57 @@ class Point2D:
     def shift(self, dx, dy):
         self.shift_x(dx) 
         self.shift_y(dy) 
-    
+
+    # TODO 
+    # write a function that calculates the distance from the point to origin (zero)
+    def distance_0(self):
+        return (self.x**2 + self.y**2)**0.5 
+
+
+
+
 p1 = Point2D(3,5) 
 p2 = Point2D(4,5)
 p1.shift_x(-1)
 p1.shift(2,1)
 print(p1)
 print([p1, p2+p1, p2+p2])
+print(p1.distance_0())
 
-for i in range(100):
-    p = Point2D(i,i+1)
-    print(p)
+#for i in range(100):
+#    p = Point2D(i,i+1)
+#    print(p)
+
+
+class Point3D(Point2D): # by inheritance from Point2D.
+    # Point2D is superclass (parent), Point3D is subclass (child)
+    def __init__(self, xx, yy, zz):
+        super().__init__(xx,yy) # Point2D creation 
+        self.z = zz
+    
+    def __str__(self): 
+        return f"(x:{self.x},y:{self.y},z:{self.z})"
+    
+    def __repr__(self): 
+        return f"Point3D({self.x},{self.y}, {self.z})"
+
+    def shift_z(self, dz):
+        self.z += dz
+    
+    def shift(self, dx, dy,dz):
+        super().shift(dx,dy) # from Point2D
+        self.shift_z(dz)     
+    
+    # TODO 
+    # write a function that calculates the distance from the point to origin (zero)
+    def distance_0(self):
+        #return (self.x**2 + self.y**2 + self.z**2)**0.5 
+
+        val = super().distance_()**2 # self.x**2 + self.y**2
+        val += self.z**2
+        return val ** 0.5
+
+     
+p4 = Point3D(1,2,3)
+p4.shift(0.4,2,-3)
+print(p4)
